648f89ec057f6c27d7e7bf071e16d8b3930d55c8
Калинкин Павел <pnzazfof@gmail.com>
2025-07-19 13:28:48 +0200

    Part 1: Exercise 1.7

--- part1/uni-cafe/src/App.jsx
+++ part1/uni-cafe/src/App.jsx
@@ -4,20 +4,41 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const handleGood = () => {
+    setTotal(total + 1)
+    setGood(good +1 )
+  }
+
+  const handleNeutral = () => {
+    setTotal(total + 1)
+    setNeutral(neutral +1 )
+  }
+
+  const handleBad = () => {
+    setTotal(total + 1)
+    setBad(bad +1 )
+  }
 
   return (
     <div>
       <h1>Give feeddback</h1>
-      
-      <button onClick={() => {setGood(good + 1)}} >Good</button>
-      <button onClick={() => {setNeutral(neutral + 1)}} >Neutral</button>
-      <button onClick={() => {setBad(bad + 1)}} >Bad</button>
+
+      <button onClick={handleGood} >Good</button>
+      <button onClick={handleNeutral} >Neutral</button>
+      <button onClick={handleBad} >Bad</button>
 
       <h1>Statistics</h1>
 
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
+      <div>
+        <div>Good: {good}</div>
+        <div>Neutral: {neutral}</div>
+        <div>Bad: {bad}</div>
+        <div>All: {total}</div>
+        <div>Average: {(good - bad)/total}</div>
+        <div>Positive: {good/total * 100} %</div>
+      </div>
     </div>
   )
 }
