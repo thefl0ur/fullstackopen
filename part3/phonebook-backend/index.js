0ab20936af3e324188e53a3e2ca37601098cc4c5
Калинкин Павел <pnzazfof@gmail.com>
2025-09-04 19:37:39 +0200

    Part 3: Exercise 3.16

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -11,14 +11,30 @@ morgan.token('body', (req) => {
     return JSON.stringify(req.body)
 })
 
+const unknownEndpointHandler = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
 const app = express()
 app.use(express.json())
 app.use(morgan(LOGGING_FROMAT))
 app.use(express.static('dist'))
 
-app.get("/api/persons", (req, resp) => {
+app.get("/api/persons", (req, resp, next) => {
     PhonebookRecord.find({}).then(
        records => { resp.json(records) }
+    ).catch(
+        (error) => next(error)
     )
 })
 
@@ -38,15 +54,17 @@ app.get("/api/persons/:id", (req, resp) => {
     resp.json(note)
 })
 
-app.delete("/api/persons/:id", (req, resp) => {
+app.delete("/api/persons/:id", (req, resp, next) => {
     PhonebookRecord.findByIdAndDelete(req.params.id).then(
         _ => {
             return resp.status(204).end()
         }
+    ).catch(
+        (error) => next(error)
     )
 })
 
-app.post("/api/persons/", (req, resp) => {
+app.post("/api/persons/", (req, resp, next) => {
     const record = new PhonebookRecord({
         "name": req.body.name,
         "number": req.body.number,
@@ -58,7 +76,12 @@ app.post("/api/persons/", (req, resp) => {
 
     record.save().then( addedRecord => {
         resp.json(addedRecord)
-    })
+    }).catch(
+        (error) => next(error)
+    )
 })
+
+app.use(unknownEndpointHandler)
+app.use(errorHandler)
 
 app.listen(PORT, () => {console.log(`Start webserver on ${PORT}`)})
