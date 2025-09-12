dfe61a2459f621b755f6e5121fb6a027e6bd5c6b
Калинкин Павел <pnzazfof@gmail.com>
2025-09-12 13:57:04 +0200

    Part 4: Exercise 4.5

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -6,6 +6,12 @@ const totalLikes = (blogs) => {
   return blogs.reduce((summ, blog) => summ + blog['likes'], 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce(
+    (favorite, current) => favorite.likes < current.likes ? current : favorite
+  )
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
