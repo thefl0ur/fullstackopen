c6c1c28cfedac98d71a851a72b7beda70dfc2277
Калинкин Павел <pnzazfof@gmail.com>
2025-09-12 13:37:41 +0200

    Part 4: Exercise 4.4

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -2,6 +2,10 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((summ, blog) => summ + blog['likes'], 0)
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
